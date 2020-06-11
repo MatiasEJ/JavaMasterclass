6ef8feb31377dc06dd8c79f8ec6de3df970ed8df
keta <don.juncos@gmail.com>
2020-06-11 16:13:06 -0300

    arrays

---
+++ src/challenge4/Main.java
@@ -0,0 +1,62 @@
+package challenge4;
+
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cantNumeros = 5, valor;
+		int[] numeros = new int[cantNumeros];
+
+		getIntegers(numeros);
+		printArray(numeros);
+		sortInteger(numeros);
+
+	}
+
+	public static void getIntegers(int[] numeros) {
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.printf("numero[%d]: ", i + 1);
+			int valor = input.nextInt();
+			numeros[i] += valor;
+		}
+		System.out.printf("-------\n");
+	}
+
+	public static void printArray(int[] nombreArray) {
+		for (int i = 0; i < nombreArray.length; i++) {
+			System.out.printf("numero[%d]: %d\n", i + 1, nombreArray[i]);
+		}
+		System.out.printf("-------\n");
+	}
+
+	public static void sortInteger(int[] nombreArray) {
+		int aux;
+		int[] nuevoArray = new int[nombreArray.length];
+
+		for (int i = 0; i < nombreArray.length; i++) {
+			nuevoArray[i] = nombreArray[i];
+		}
+
+		boolean flag = true;
+		int temp;
+		while (flag) {
+			flag = false;
+			for (int i = 0; i < nuevoArray.length - 1; i++) {
+				if (nuevoArray[i] < nuevoArray[i + 1]) {
+					temp = nuevoArray[i];
+					nuevoArray[i] = nuevoArray[i + 1];
+					nuevoArray[i + 1] = temp;
+					flag = true;
+
+				}
+			}
+		}
+
+		printArray(nuevoArray);
+
+	}
+
+}
--- src/challenge4/package-info.java
+++ src/challenge4/package-info.java
@@ -1,1 +1,15 @@
 package challenge4;
+
+//CHALLENGE ARRAYS
+/**
+ ** Create a program using arrays that sorts a list of integers in descending
+ * order. Descending order es highest value to lowest. Set up the program so
+ * that the numbers to sort are read in from the keyboard. Imprement the
+ * following methods getIntegers, printArray and sortIntegers getIntegers
+ * returns an array of entered integers from keyboard printArray prints out the
+ * contents of the array and sortIntegers shoul sort the array and return a new
+ * array containing the sorted numbers you will have to figure out how to copy
+ * the array elements from the passed array into a new array and sort them and
+ * return the new sorted array
+ *
+ ***/
